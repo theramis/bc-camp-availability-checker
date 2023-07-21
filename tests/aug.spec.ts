8eb97dd50d1ee208bc875c0c3476d39c53f9acdc
Simar Kalra <theramis@users.noreply.github.com>
2023-07-20 18:09:10 -0700

    Add nairn falls

--- tests/aug.spec.ts
+++ tests/aug.spec.ts
@@ -15,6 +15,10 @@ test("Alice Lake", async ({ page }) => {
   await checkPage("Alice Lake", "https://camping.bcparks.ca/create-booking/results?resourceLocationId=-2147483647&mapId=-2147483648&searchTabGroupId=0&bookingCategoryId=0&startDate=2023-08-12&endDate=2023-08-13&nights=1&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=1&filterData=%7B%7D&equipmentCapacity=1", page);
 });
 
+test("Nairn Falls", async ({ page }) => {
+  await checkPage("Nairn Falls", "https://camping.bcparks.ca/create-booking/results?resourceLocationId=-2147483564&mapId=-2147483471&searchTabGroupId=0&bookingCategoryId=0&startDate=2023-08-12&endDate=2023-08-13&nights=1&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=1&filterData=%7B%7D&equipmentCapacity=1", page);
+});
+
 async function checkPage(name: string, link: string, page: Page) {
   await page.goto(
     link
